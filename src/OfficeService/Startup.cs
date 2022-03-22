22d8b9f5e3e1a97dbc930d736467a4080e417070
ndoriomedov <doriomedov213@gmail.com>
2022-03-22 20:59:21 +0300

    update spec version

--- src/OfficeService/Startup.cs
+++ src/OfficeService/Startup.cs
@@ -51,7 +51,7 @@ namespace LT.DigitalOffice.OfficeService
         .GetSection(BaseServiceInfoConfig.SectionName)
         .Get<BaseServiceInfoConfig>();
 
-      Version = "1.0.0.0";
+      Version = "1.0.1.0";
       Description = "OfficeService is an API that intended to work with offices.";
       StartTime = DateTime.UtcNow;
       ApiName = $"LT Digital Office - {_serviceInfoConfig.Name}";
