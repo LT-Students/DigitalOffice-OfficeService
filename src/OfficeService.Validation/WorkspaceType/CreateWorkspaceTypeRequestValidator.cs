8b172bd1dc299675fbfc5eb03a14cb4284d4366c
Dmitry Solovyov <dmitry.solovyov@lanit-tercom.com>
2022-04-04 13:58:20 +0300

    add dummies for request validators of workspaces and workspace types

---
+++ src/OfficeService.Validation/WorkspaceType/CreateWorkspaceTypeRequestValidator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using LT.DigitalOffice.OfficeService.Data.WorkspaceType.Interfaces;
+using LT.DigitalOffice.OfficeService.Models.Dto.Requests.WorkspaceType;
+using LT.DigitalOffice.OfficeService.Validation.WorkspaceType.Interfaces;
+
+namespace LT.DigitalOffice.OfficeService.Validation.WorkspaceType
+{
+  public class CreateWorkspaceTypeRequestValidator : AbstractValidator<CreateWorkspaceTypeRequest>, ICreateWorkspaceTypeRequestValidator
+  {
+    public CreateWorkspaceTypeRequestValidator(IWorkspaceTypeRepository repository)
+    {
+      // TODO: Add rules
+    }
+  }
+}
---
+++ src/OfficeService.Validation/WorkspaceType/Interfaces/ICreateWorkspaceTypeRequestValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+using LT.DigitalOffice.Kernel.Attributes;
+using LT.DigitalOffice.OfficeService.Models.Dto.Requests.WorkspaceType;
+
+namespace LT.DigitalOffice.OfficeService.Validation.WorkspaceType.Interfaces
+{
+  [AutoInject]
+  public interface ICreateWorkspaceTypeRequestValidator : IValidator<CreateWorkspaceTypeRequest>
+  {
+  }
+}
