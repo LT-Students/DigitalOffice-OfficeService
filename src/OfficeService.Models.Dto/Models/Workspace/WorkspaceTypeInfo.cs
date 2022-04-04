e1d16878991bc9c1fff5a5dc990ed1a0524a12df
Dmitry Solovyov <dmitry.solovyov@lanit-tercom.com>
2022-04-04 13:51:04 +0300

    add DTO entities for workspaces and workspace types

---
+++ src/OfficeService.Models.Dto/Models/Workspace/WorkspaceTypeInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LT.DigitalOffice.OfficeService.Models.Dto.Models.Workspace
+{
+  public record WorkspaceTypeInfo
+  {
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+    public TimeOnly? StartTime { get; set; }
+
+    public TimeOnly? EndTime { get; set; }
+
+    public int BookingRule { get; set; }
+
+    public bool IsActive { get; set; }
+  }
+}
