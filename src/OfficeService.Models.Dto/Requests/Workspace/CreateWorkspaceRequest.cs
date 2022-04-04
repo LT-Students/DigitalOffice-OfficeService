902cb71ddecdf0559383870f8002e945c41c127d
Dmitry Solovyov <dmitry.solovyov@lanit-tercom.com>
2022-04-04 13:57:09 +0300

    add filters and requests for workspaces and workspace types

---
+++ src/OfficeService.Models.Dto/Requests/Workspace/CreateWorkspaceRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using LT.DigitalOffice.OfficeService.Models.Dto.Requests.WorkspaceType;
+
+namespace LT.DigitalOffice.OfficeService.Models.Dto.Requests.Workspace
+{
+  public record CreateWorkspaceRequest
+  {
+    public Guid ParentId { get; set; }
+
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+    public Guid WorkspaceTypeId { get; set; }
+
+    public CreateWorkspaceTypeRequest WorkspaceType { get; set; }
+  }
+}
---
+++ src/OfficeService.Models.Dto/Requests/WorkspaceType/CreateWorkspaceTypeRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LT.DigitalOffice.OfficeService.Models.Dto.Requests.WorkspaceType
+{
+  public record CreateWorkspaceTypeRequest
+  {
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+    public TimeOnly? StartTime { get; set; }
+
+    public TimeOnly? EndTime { get; set; }
+
+    public int BookingRule { get; set; }
+  }
+}
