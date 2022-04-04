c6ddbc86a0a70843093d48a7f22161d86f326242
Dmitry Solovyov <dmitry.solovyov@lanit-tercom.com>
2022-04-04 13:58:30 +0300

    add controllers for workspaces and workspace types

---
+++ src/OfficeService/Controllers/WorkspaceController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using LT.DigitalOffice.Kernel.Responses;
+using LT.DigitalOffice.OfficeService.Business.Commands.Workspace.Interfaces;
+using LT.DigitalOffice.OfficeService.Models.Dto.Models.Workspace;
+using LT.DigitalOffice.OfficeService.Models.Dto.Requests.Workspace;
+using LT.DigitalOffice.OfficeService.Models.Dto.Requests.Workspace.Filters;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace LT.DigitalOffice.OfficeService.Controllers
+{
+  [Route("[controller]")]
+  [ApiController]
+  public class WorkspaceController : ControllerBase
+  {
+    [HttpPost("create")]
+    public async Task<OperationResultResponse<Guid>> CreateAsync(
+      [FromServices] ICreateWorkspaceCommand  command,
+      [FromBody] CreateWorkspaceRequest request)
+    {
+      return await command.ExecuteAsync(request);
+    }
+
+    [HttpGet("find")]
+    public async Task<FindResultResponse<WorkspaceInfo>> FindAsync(
+      [FromServices] IFindWorkspacesCommand command,
+      [FromQuery] WorkspaceFindFilter filter)
+    {
+      return await command.ExecuteAsync(filter);
+    }
+  }
+}
