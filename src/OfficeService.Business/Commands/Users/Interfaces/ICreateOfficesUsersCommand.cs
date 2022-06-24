784a9f857da857ae76f547821596259cdc806ed6
Dmitry Solovyov <dmitry.solovyov@lanit-tercom.com>
2022-06-24 12:48:37 +0300

    fix

--- src/OfficeService.Business/Commands/Users/Interfaces/ICreateOfficesUsersCommand.cs
+++ src/OfficeService.Business/Commands/Users/Interfaces/ICreateOfficesUsersCommand.cs
@@ -1,9 +1,11 @@
 ﻿using System.Threading.Tasks;
+using LT.DigitalOffice.Kernel.Attributes;
 using LT.DigitalOffice.Kernel.Responses;
 using LT.DigitalOffice.OfficeService.Models.Dto.Requests.Users;
 
 namespace LT.DigitalOffice.OfficeService.Business.Commands.Users.Interfaces
 {
+  [AutoInject]
   public interface ICreateOfficesUsersCommand
   {
     Task<OperationResultResponse<bool>> ExecuteAsync(CreateOfficesUsersRequest request);
