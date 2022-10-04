b30dbd97263a15d7ad9b0798dcabb5b06b7f5eeb
Dmitry Solovyov <dmitry.solovyov@lanit.tercom.com>
2022-10-04 18:54:16 +0300

    remove mapper

--- src/OfficeService.Business/Commands/Office/Find/FindOfficesHandler.cs
+++ src/OfficeService.Business/Commands/Office/Find/FindOfficesHandler.cs
@@ -2,59 +2,60 @@
 using System.Threading;
 using System.Threading.Tasks;
 using LT.DigitalOffice.Kernel.Responses;
-using LT.DigitalOffice.OfficeService.Business.Commands.Office.Interfaces;
 using LT.DigitalOffice.OfficeService.Data.Provider;
-using LT.DigitalOffice.OfficeService.Mappers.Models.Interfaces;
-using LT.DigitalOffice.OfficeService.Models.Db;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 
-namespace LT.DigitalOffice.OfficeService.Business.Commands.Office
+namespace LT.DigitalOffice.OfficeService.Business.Commands.Office.Find
 {
   public class FindOfficesHandler : IRequestHandler<OfficeFindFilter, FindResultResponse<OfficeInfo>>
   {
     private readonly IDataProvider _provider;
-    private readonly IOfficeInfoMapper _mapper;
 
     public FindOfficesHandler(
-      IDataProvider provider,
-      IOfficeInfoMapper mapper)
+      IDataProvider provider)
     {
       _provider = provider;
-      _mapper = mapper;
     }
 
     public async Task<FindResultResponse<OfficeInfo>> Handle(OfficeFindFilter filter, CancellationToken ct)
     {
-      IQueryable<DbOffice> dbOffices = _provider.Offices.AsQueryable();
+      IQueryable<OfficeInfo> offices = _provider.Offices.Select(o => new OfficeInfo
+      {
+        Id = o.Id,
+        Name = o.Name,
+        City = o.City,
+        Address = o.Address,
+        Latitude = o.Latitude,
+        Longitude = o.Longitude,
+        IsActive = o.IsActive
+      });
 
       if (filter.IsActive.HasValue)
       {
-        dbOffices = dbOffices.Where(x => x.IsActive == filter.IsActive);
+        offices = offices.Where(x => x.IsActive == filter.IsActive);
       }
 
       if (!string.IsNullOrWhiteSpace(filter.NameIncludeSubstring))
       {
-        dbOffices = dbOffices.Where(x =>
+        offices = offices.Where(x =>
           x.Name.Contains(filter.NameIncludeSubstring));
       }
 
       if (filter.IsAscendingSort.HasValue)
       {
-        dbOffices = filter.IsAscendingSort.Value
-          ? dbOffices.OrderBy(o => o.Name)
-          : dbOffices.OrderByDescending(o => o.Name);
+        offices = filter.IsAscendingSort.Value
+          ? offices.OrderBy(o => o.Name)
+          : offices.OrderByDescending(o => o.Name);
       }
 
       return new FindResultResponse<OfficeInfo>
       {
-        TotalCount = await dbOffices.CountAsync(ct),
-        Body = dbOffices
+        TotalCount = await offices.CountAsync(ct),
+        Body = await offices
           .Skip(filter.SkipCount)
           .Take(filter.TakeCount)
-          .AsEnumerable()
-          .Select(_mapper.Map)
-          .ToList()
+          .ToListAsync(ct)
       };
     }
   }
