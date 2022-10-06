f4dfa803aaa2372e5f1412ad1469becb8a83db3c
Dmitry Solovyov <dmitry.solovyov@lanit.tercom.com>
2022-10-06 21:12:37 +0300

    remove mapper method

--- src/OfficeService.Mappers/Db/DbOfficeUserMapper.cs
+++ src/OfficeService.Mappers/Db/DbOfficeUserMapper.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using LT.DigitalOffice.Kernel.Extensions;
-using LT.DigitalOffice.Models.Broker.Publishing.Subscriber.Office;
 using LT.DigitalOffice.OfficeService.Mappers.Db.Interfaces;
 using LT.DigitalOffice.OfficeService.Models.Db;
 using LT.DigitalOffice.OfficeService.Models.Dto.Requests.Users;
@@ -16,24 +15,6 @@ namespace LT.DigitalOffice.OfficeService.Mappers.Db
     public DbOfficeUserMapper(IHttpContextAccessor httpContextAccessor)
     {
       _httpContextAccessor = httpContextAccessor;
-    }
-
-    public DbOfficeUser Map(ICreateUserOfficePublish request)
-    {
-      if (request == null)
-      {
-        return null;
-      }
-
-      return new DbOfficeUser
-      {
-        Id = Guid.NewGuid(),
-        OfficeId = request.OfficeId,
-        UserId = request.UserId,
-        CreatedAtUtc = DateTime.UtcNow,
-        CreatedBy = request.CreatedBy,
-        IsActive = true
-      };
     }
 
     public List<DbOfficeUser> Map(CreateOfficeUsers request)
