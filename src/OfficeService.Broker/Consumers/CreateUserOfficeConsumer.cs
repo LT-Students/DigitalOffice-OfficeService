f23fce1fcb3ab3c8444f08309c81a128a4b64e36
Dmitry Solovyov <dmitry.solovyov@lanit.tercom.com>
2022-10-06 21:13:06 +0300

    update existing consumer according to new standarts

--- src/OfficeService.Broker/Consumers/CreateUserOfficeConsumer.cs
+++ src/OfficeService.Broker/Consumers/CreateUserOfficeConsumer.cs
@@ -1,37 +1,61 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using LT.DigitalOffice.Kernel.RedisSupport.Helpers.Interfaces;
 using LT.DigitalOffice.Models.Broker.Publishing.Subscriber.Office;
-using LT.DigitalOffice.OfficeService.Data.Interfaces;
-using LT.DigitalOffice.OfficeService.Mappers.Db.Interfaces;
+using LT.DigitalOffice.OfficeService.Data.Provider;
+using LT.DigitalOffice.OfficeService.Models.Db;
 using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace LT.DigitalOffice.OfficeService.Broker.Consumers
 {
   public class CreateUserOfficeConsumer : IConsumer<ICreateUserOfficePublish>
   {
-    private readonly IOfficeRepository _officeRepository;
-    private readonly IOfficeUserRepository _officeUserRepository;
-    private readonly IDbOfficeUserMapper _officeUserMapper;
+    private readonly IDataProvider _provider;
     private readonly IGlobalCacheRepository _globalCache;
+    private readonly ILogger<CreateUserOfficeConsumer> _logger;
 
     public CreateUserOfficeConsumer(
-      IOfficeRepository officeRepository,
-      IOfficeUserRepository officeUserRepository,
-      IDbOfficeUserMapper officeUserMapper,
-      IGlobalCacheRepository globalCache)
+      IDataProvider provider,
+      IGlobalCacheRepository globalCache,
+      ILogger<CreateUserOfficeConsumer> logger)
     {
-      _officeRepository = officeRepository;
-      _officeUserRepository = officeUserRepository;
-      _officeUserMapper = officeUserMapper;
+      _provider = provider;
       _globalCache = globalCache;
+      _logger = logger;
     }
 
     public async Task Consume(ConsumeContext<ICreateUserOfficePublish> context)
     {
-      if (await _officeRepository.DoesExistAsync(context.Message.OfficeId))
+      ICreateUserOfficePublish publish = context.Message;
+      if (await _provider.Offices.AnyAsync(o => o.Id == publish.OfficeId))
       {
-        await _officeUserRepository.CreateAsync(_officeUserMapper.Map(context.Message));
-        await _globalCache.RemoveAsync(context.Message.OfficeId);
+        if (publish is null)
+        {
+          return;
+        }
+
+        DbOfficeUser user = new DbOfficeUser
+        {
+          Id = Guid.NewGuid(),
+          OfficeId = publish.OfficeId,
+          UserId = publish.UserId,
+          CreatedAtUtc = DateTime.UtcNow,
+          CreatedBy = publish.CreatedBy,
+          IsActive = publish.IsActive
+        };
+
+        await _provider.OfficesUsers.AddAsync(user);
+
+        if (publish.IsActive)
+        {
+          await _globalCache.RemoveAsync(publish.OfficeId);
+        }
+      }
+      else
+      {
+        _logger.LogError($"Cannot create user with Id {publish.UserId} in office with Id {publish.OfficeId}");
       }
     }
   }
