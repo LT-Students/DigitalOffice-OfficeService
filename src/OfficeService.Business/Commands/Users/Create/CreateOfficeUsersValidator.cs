d7318f022a928e073ef1b1a23919973ceb22ff62
Dmitry Solovyov <dmitry.solovyov@lanit.tercom.com>
2022-10-18 16:50:43 +0300

    rework office user controller

--- src/OfficeService.Business/Commands/Users/Create/CreateOfficeUsersValidator.cs
+++ src/OfficeService.Business/Commands/Users/Create/CreateOfficeUsersValidator.cs
@@ -2,12 +2,10 @@
 using FluentValidation;
 using LT.DigitalOffice.OfficeService.Broker.Requests.Interfaces;
 using LT.DigitalOffice.OfficeService.Data.Interfaces;
-using LT.DigitalOffice.OfficeService.Models.Dto.Requests.Users;
-using LT.DigitalOffice.OfficeService.Validation.Users.Interfaces;
 
-namespace LT.DigitalOffice.OfficeService.Validation.Users
+namespace LT.DigitalOffice.OfficeService.Business.Commands.Users.Create
 {
-  public class CreateOfficeUsersValidator : AbstractValidator<CreateOfficeUsers>, ICreateOfficeUsersValidator
+  public class CreateOfficeUsersValidator : AbstractValidator<CreateOfficeUsersRequest>
   {
     public CreateOfficeUsersValidator(
       IOfficeRepository officeRepository,
@@ -18,7 +16,7 @@ namespace LT.DigitalOffice.OfficeService.Validation.Users
         .WithMessage("Office must exist.");
 
       RuleFor(r => r.UsersIds)
-        .MustAsync(async (usersIds, _) => 
+        .MustAsync(async (usersIds, _) =>
           (await userService.CheckUsersExistence(usersIds, new List<string>())).Count == usersIds.Count)
         .WithMessage("Users must exist");
     }
